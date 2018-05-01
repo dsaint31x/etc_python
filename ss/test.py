c8442532946837c4d9ec0f0a3cf3f992f206da31
Hang-Keun Kim <dsaint31@gachon.ac.kr>
2018-05-01 15:57:07 +0900

    Create test.py

---
+++ ss/test.py
@@ -0,0 +1,21 @@
+from scipy.fftpack import fft
+
+# Number of sample points
+N = 600
+
+# sample spacing
+T = 1.0 / 800.0
+
+x = np.linspace(0.0, N*T, N)
+
+
+
+y = 2.*np.cos( 3/(2.0*np.pi)*2.0*np.pi*x)
+
+yf = fft(y)
+xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+
+import matplotlib.pyplot as plt
+plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+plt.grid()
+plt.show()
